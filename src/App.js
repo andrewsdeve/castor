ab2f428a3ba3f7c778f1e1231f957ca628edcc90
andrewsdeve <andrewszaratema@gmail.com>
2022-10-25 17:30:36 -0500

    PreEntrega1MartinezZarateCorrergido

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import NavBar from './components/NavBar'
+import ItemListContainer from './components/ItemListContainer'
 import React from 'react'
+
 
 
 const App = () => {
   return (
     <>
       <NavBar/>
+      <ItemListContainer texto = 'Aqui va mi catalago de productos' />
       
     </>
   )
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,38 @@
 import React from 'react'
 import CartWidget from './CartWidget.js'
+import { FaApple} from 'react-icons/fa'
+
 
 const NavBar = () => {
   return (
     <>
+      
       <div className='Container'>
-        <CartWidget/>
+        <div className='LogoContainer'>
+            <FaApple/>
+            <p>Castor-</p>
+            <p>Online</p>
+            <ul className='Menu'>
+              <li className='MenuItem'>
+                <a className='MenuItemLink' href="https://www.apple.com/">iPhone</a>
+              </li>
+              <li className='MenuItem'>
+                <a className='MenuItemLink' href="https://www.apple.com/">iPad</a>
+              </li>
+              <li className='MenuItem'>
+                <a className='MenuItemLink' href="https://www.apple.com/">Mac</a>
+              </li>
+              <li className='MenuItem'>
+                <a className='MenuItemLink' href="https://www.apple.com/">AirPods</a>
+              </li><li className='MenuItem'>
+                <a className='MenuItemLink' href="https://www.apple.com/">Watch</a>
+              </li>
+              <li className='MenuItem'>
+                <a className='MenuItemLink' href="https://www.apple.com/">Accesories</a>
+              </li>
+            </ul>
+            <CartWidget/>
+        </div>
       </div>
     </>
 
